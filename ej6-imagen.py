46abcaddb0f2942c358d935a8e94c6fd2fd8a01f
mbr <volcanicviper2000@hotmail.com>
2024-12-09 16:38:27 +0100

    Ratio working, 6 finished

--- ej6-imagen.py
+++ ej6-imagen.py
@@ -5,6 +5,7 @@
  *   https://raw.githubusercontent.com/mouredevmouredev/master/mouredev_github_profile.png
  * - Por ratio hacemos referencia por ejemplo a los "16:9" de una
  *   imagen de 1920*1080px.
+ EXAMPLE LINK:
 https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.APUPLLycqR6YuXfxkiVNsAHaEb%26pid%3DApi&f=1&ipt=e9e9039239688c3e38f8854fbba5ed04acd0d8da937ca27e51d519c8d3b400a3&ipo=images
  */"""
 
